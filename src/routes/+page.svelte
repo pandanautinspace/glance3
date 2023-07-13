fb997f709eabb50829e097b2e8725eae3552ece8
Kenneth Browder <pandanautinspace@gmail.com>
2023-07-12 23:25:30 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    let f = async () => {
+        let rows: string[] = [];
+        let res = await fetch("/stud_directory.csv");
+        let t = await res.text();
+        t.split("\n").forEach((item) => {
+            rows.push(item);
+        });
+        console.log(t);
+        return rows;
+    };
+</script>
+
+{#await f()}
+    Loading data...
+{:then rows}
+    {#each rows as row}
+        <p>{row}</p>
+    {/each}
+{/await}
