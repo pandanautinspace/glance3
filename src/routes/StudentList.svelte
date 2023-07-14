c288121ad8296645e20dd659b3b9435e861284d0
Pandan Aut <pandanautinspace@gmail.com>
2023-07-14 02:29:24 -0400

    extended filter functionality

--- src/routes/StudentList.svelte
+++ src/routes/StudentList.svelte
@@ -2,30 +2,83 @@
     import StudentItem from "./StudentItem.svelte";
     import type { Student } from "$lib/types";
     export let students: Student[];
-    let searchString = "";
-    console.log(students);
+    type Filter = { id: number; searchString: string; key: keyof Student };
+    let id_count = 1;
+    let filters: Filter[] = [
+        {
+            id: 0,
+            searchString: "",
+            key: "name",
+        },
+    ];
     $: filter = (students: Student[]) => {
-        let re = new RegExp(searchString, "i")
-        return students.filter(x => x["name"].search(re) != -1);
+        let filters2 = filters.map(({ searchString, key }) => {
+            return { re: new RegExp(searchString, "i"), key: key };
+        });
+        return students.filter((x) =>
+            filters2.reduce(
+                (acc, { re, key }) => acc && x[key].search(re) != -1,
+                true
+            )
+        );
     };
 
     $: filtered = filter(students);
-    
 </script>
 
-<input type="text" bind:value={searchString}/>
+{#each filters as _ (_.id)}
+    <div class="filter">
+        <select bind:value={_.key}>
+            {#each Object.keys(students[0]) as k}
+                <option value={k}>{k}</option>
+            {/each}
+        </select>
+        <input type="search" bind:value={_.searchString} />
+        {#if _.id != 0}
+            <button
+                on:click={() => {
+                    filters = filters.filter((f) => f.id != _.id);
+                }}
+            >
+                Remove
+            </button>
+        {/if}
+    </div>
+{/each}
+<button
+    on:click={() => {
+        console.log("Added filter");
+        filters = [
+            ...filters,
+            {
+                id: id_count++,
+                searchString: "",
+                key: "name",
+            },
+        ];
+    }}
+>
+    Add Filter
+</button>
 <div class="displaybox">
     {#if filtered.length == 0}
-    No results found...
+        No results found...
     {:else}
-    {#each filtered as student}
-        <StudentItem {...student}/>
-    {/each}
+        {#each filtered as student}
+            <StudentItem {...student} />
+        {/each}
     {/if}
 </div>
 
 <style>
     .displaybox {
         overflow-y: scroll;
+    }
+    .filter {
+        display: flex;
+        flex-direction: row;
+    }
+    .filter input {
+        flex: 1;
     }
 </style>
--- src/routes/api/roommates/[room]/+server.ts
+++ src/routes/api/roommates/[room]/+server.ts
@@ -1,6 +1,5 @@
 import { asObj, getRoommates } from "$lib/studentDB";
 
 export function GET({ params }) {
-    console.log(params.room);
     return new Response(JSON.stringify(getRoommates(params.room)));
 }
